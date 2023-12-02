910fff5ab84d885611661d8b339a3f2e5c5fe94c
Bohdan Liashuk <luashuk2002@gmail.com>
2023-12-02 21:16:56 +0200

    Create fun loadRandomQuote

--- app/src/main/java/com/example/randomquote/MainActivity.kt
+++ app/src/main/java/com/example/randomquote/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.example.randomquote
 
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -13,6 +19,23 @@ class MainActivity : AppCompatActivity() {
 
     val quoteApiService = retrofit.create(QuoteApiService::class.java)
 
+    private lateinit var quoteTextView: TextView
+
+    private fun loadRandomQuote() {
+        GlobalScope.launch(Dispatchers.Main) {
+            val response = withContext(Dispatchers.IO) {
+                quoteApiService.getRandomQuote()
+            }
+
+            if (response.isSuccessful) {
+                val quote = response.body()
+                quoteTextView.text = "\"${quote?.quoteText}\" - ${quote?.author}"
+            } else {
+                // Обработка ошибок при запросе цитаты
+                Toast.makeText(this@MainActivity, "Ошибка загрузки цитаты", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
