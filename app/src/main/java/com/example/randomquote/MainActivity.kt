bb810fdcd5aa14cad359e1d792ba638ad861a367
Bohdan Liashuk <luashuk2002@gmail.com>
2023-12-02 21:10:58 +0200

    Initialization Retrofit

--- app/src/main/java/com/example/randomquote/MainActivity.kt
+++ app/src/main/java/com/example/randomquote/MainActivity.kt
@@ -2,8 +2,17 @@ package com.example.randomquote
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
+    val retrofit = Retrofit.Builder()
+        .baseUrl("https://quote-garden.herokuapp.com")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    val quoteApiService = retrofit.create(QuoteApiService::class.java)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
